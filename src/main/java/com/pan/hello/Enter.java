0ca90f95a1ab40b59090217745a59f1bd0dc2b92
jpan <j.pan.0923551124@gmail.com>
2021-11-13 00:40:45 +0800

    third commit

---
+++ src/main/java/com/pan/hello/Enter.java
@@ -0,0 +1,18 @@
+package com.pan.hello;
+
+public class Enter {
+    public static void main(String[] args) {
+        System.out.println("Please enter your age");
+        int age = 8;
+        age = 22;
+        System.out.println(age);
+        float weight = 49.5f;
+        System.out.println(weight);
+        char grading = 'B';
+        System.out.println(grading);
+        byte b = 127;
+        System.out.println(b);
+        char lastName = '潘';
+        System.out.println(lastName);
+    }
+}

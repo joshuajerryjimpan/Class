d0972ef79e48ee8a66ef82385f3e103f5c530cab
jpan <j.pan.0923551124@gmail.com>
2021-12-06 20:30:34 +0800

    tenth commit

--- src/main/java/com/pan/train/Trainning2.java
+++ src/main/java/com/pan/train/Trainning2.java
@@ -1,12 +1,25 @@
 package com.pan.train;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Trainning2 {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 20; j++) {
-                System.out.print("*");
+        Random random = new Random();
+        int secret = random.nextInt(11);
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 3; i++){
+            System.out.println("Please enter the number(from 1 to 10)");
+            String string = scanner.next();
+            int number = Integer.parseInt(string);
+            if (number > secret){
+                System.out.println("Smaller");
+            }else if (number == secret){
+                System.out.println("Bingo");
+                break;
+            }else if (number < secret){
+                System.out.println("Bigger");
             }
-            System.out.println();
         }
     }
 }
---
+++ src/main/java/com/pan/train/Trainning3.java
@@ -0,0 +1,12 @@
+package com.pan.train;
+
+public class Trainning3 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 20; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

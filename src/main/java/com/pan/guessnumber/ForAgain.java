a24f51b2e12b8dc431a22a6bcce389b70ae8e4e9
jpan <j.pan.0923551124@gmail.com>
2021-12-07 11:01:35 +0800

    twelfth commit

--- src/main/java/com/pan/guessnumber/ForAgain.java
+++ src/main/java/com/pan/guessnumber/ForAgain.java
@@ -1,34 +1,34 @@
-package com.pan.train;
+package com.pan.guessnumber;
 
 import java.util.Random;
 import java.util.Scanner;
 
-public class Trainning2 {
+public class ForAgain {
     public static void main(String[] args) {
         Random random = new Random();
-        int secret = random.nextInt(100) + 1;
+        int secret = random.nextInt(100)+1;
         System.out.println(secret);
         int min = 1;
         int max = 100;
         boolean win = false;
-        while (win) {
-            System.out.println("Please enter your number from " + min + " to " + max);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Please enter your number from "+min+" to "+max);
             Scanner scanner = new Scanner(System.in);
             String string = scanner.next();
             int num = Integer.parseInt(string);
-            if (num < secret) {
+            if (num < secret){
                 System.out.println("Bigger");
                 min = num;
-            } else if (num > secret) {
+            }else if (num > secret){
                 System.out.println("Smaller");
                 max = num;
-            } else if (num == secret) {
+            }else if (num == secret){
                 win = true;
             }
         }
-        if (win) {
+        if (win){
             System.out.println("YOU WIN !!");
-        } else {
+        }else {
             System.out.println("YOU LOSE !!");
         }
     }
--- src/main/java/com/pan/train/Trainning2_wrong.java
+++ src/main/java/com/pan/train/Trainning2_wrong.java
@@ -1,36 +1,34 @@
 package com.pan.train;
 
-
 import java.util.Random;
 import java.util.Scanner;
 
-public class Trainning1 {
+public class Trainning2_wrong {
     public static void main(String[] args) {
         Random random = new Random();
-        int secret =random.nextInt(100)+1;
+        int secret = random.nextInt(100) + 1;
         System.out.println(secret);
-        int max = 100;
         int min = 1;
+        int max = 100;
         boolean win = false;
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Please guess the number from "+min+" to "+max);
+        while (win) {
+            System.out.println("Please enter your number from " + min + " to " + max);
             Scanner scanner = new Scanner(System.in);
             String string = scanner.next();
             int num = Integer.parseInt(string);
-            if (num > secret){
-                System.out.println("smaller");
-                max = num;
-            }else if (num == secret){
-                win = true;
-                break;
-            }else if (num < secret){
+            if (num < secret) {
                 System.out.println("Bigger");
                 min = num;
+            } else if (num > secret) {
+                System.out.println("Smaller");
+                max = num;
+            } else if (num == secret) {
+                win = true;
             }
         }
-        if (win){
+        if (win) {
             System.out.println("YOU WIN !!");
-        }else {
+        } else {
             System.out.println("YOU LOSE !!");
         }
     }
